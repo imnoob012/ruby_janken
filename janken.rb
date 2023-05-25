17052990fa2eba64bae04599cee48482d9de5b2b
Ritsuki <121638860+imnoob012@users.noreply.github.com>
2023-05-26 03:12:41 +0900

    Update janken.rb

--- janken.rb
+++ janken.rb
@@ -1,56 +1,68 @@
-#じゃんけん
-def janken
- 
- puts "じゃんけん....."
- puts "0(グー)1(チョキ)2(パー)"
- 
- janken = ["グー", "チョキ", "パー"]
+class Game
+  puts "じゃんけん..."
+  
+  def janken
+    puts "0(グー)1(チョキ)2(パー)"
 
- player_hand = gets.chomp.to_i
- program_hand = rand(3)
+     player_hand = gets.chomp.to_i
+     program_hand = rand(3)
+     janken = ["グー","チョキ","パー"]
 
- puts "あなたの手:#{janken[player_hand]} 相手の手:#{janken[program_hand]}"
+    puts "あなた : #{janken[player_hand]}を出しました"
+    puts "相手 : #{janken[program_hand]}を出しました"
+    
+    if player_hand == program_hand
+      puts "あいこで"
+      return true
+    elsif (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
+      puts "あなたの勝ちです"
+      $more_game = "win"
+      return false
+    else 
+      puts "あなたの負けです"
+      $more_game = "lose"
+      return false
+    end
+  end
+    
+  
+  def look_this_way
+    puts "あっち向いて..."
+    puts "0(上)1(右)2(下)3(左)"
+    
+    player_direction = gets.chomp.to_i
+    program_direction = rand(4)
+    direction = ["上","右","下","左"]
 
- if player_hand == program_hand
- puts "あいこで"
- return true
- elsif(player_hand == 0 && program_hand == 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
- @win = "win"
- look_this_way
- return false
- else
- @lose = "lose"
- look_this_way
- return false
- end
-end
-
-#あっち向いてホイ
-def look_this_way
- puts "あっち向いて・・・"
- 
- look_type = ["上", "右", "下", "左"]
- 
- puts "[0]上\n[1]右\n[2]下\n[3]左"
- 
- player_look = gets.chomp.to_i
- program_look = rand(4)
-
- puts "あなた:#{look_type[player_look]}\n相手：#{look_type[program_look]}"
-
-if (player_look == program_look && @win = "win")
- puts "あなたの勝ちです！！"
-elsif  (player_look == program_look && @lose = "lose")
- puts "あなたの負けです。。"
-else
- janken
-end
-
-end
-
-next_game = true
-while next_game
- next_game = janken
-end
+    puts "あなた : #{direction[player_direction]}"
+    puts "相手 : #{direction[program_direction]}"
+    
+    
+   if player_direction == program_direction
+        case $more_game
+        when "win"
+          puts "あなたの勝ちです"
+          return false
+        when "lose"
+          puts "あなたの負けです"
+          return false
+        end
+   else
+      puts "勝敗が決まりません"
+      return true
+    end
+   end
+  end
+  
+  game = Game.new
+  next_game = true
+  while next_game
+    result = game.janken
+    if result
+      result = game.janken
+    else
+      next_game = game.look_this_way
+    end
+  end
 
 
